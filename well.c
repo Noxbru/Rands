941573b0304bac00fc7cae880859025e3220cf94
Noxbru <brunojimen@gmail.com>
2013-02-26 19:51:23 +0100

    Added an explanation to WELL algorithm.
    It mainly focus on WELL 512, which is the
    one I have implemented, maybe now that I
    understand how they work I can implement
    also WELL 1024... we'll see.

--- well.c
+++ well.c
@@ -3,6 +3,52 @@
  * Chris Lomont. It is released in the public
  * domain, but he'd appreciate a reference, so
  * here it is (^_^)                                 */
+
+/* Here is the explanation of the algorithm as I have
+ * managed to understand. This corresponds to
+ * WELL512, but it may be used in combination to
+ * the paper to fully understand an example and
+ * implement the others.
+ * 
+ * All the operations are performed modulo 16, so
+ * for example i+13 has to be understood as
+ * (i+13)%16 or as in the code (i+13)&15
+ * 
+ * We will use four variables, one for each of the
+ * numbers needed for the final calculation of the
+ * state.
+ * 
+ * ----------------------------------------------------------------
+ * a = state[ind]
+ * b = state[ind+13]
+ * c = state[ind+9]
+ * 
+ * a = a^(a<<16)^b^(b<<15)      (z1)
+ * c = c^(c>>11)                (z2)
+ * d = a^c                      (z3)
+ * 
+ * state[ind] = d
+ * ind = ind - 1
+ * b = state[ind]               (z0)
+ * 
+ * state[ind] = b^(b^<<2)^a^(a<<18)^(c<<28)^d^((d<<5)&0xda442d24UL)
+ * 
+ * return state[ind]
+ * ----------------------------------------------------------------
+ * 
+ * The original code from Francois Panneton, Pierre
+ * L'Ecuyer and Makoto Matsumoto mainly does the
+ * same but performs the ind-- as the last step
+ * doing some more computations for getting z0
+ * and the new state. We also return an unsigned int
+ * instead of a double, so it's the user who can
+ * choose what to do with the final number
+ *
+ * The original code can be taken from here:
+ * http://www.iro.umontreal.ca/~panneton/WELLRNG.html
+ */
+
+
 #include <stdlib.h>
 #include "rands.h"
 
