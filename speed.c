a1758e16e0a1e4676315b24d582d4aeb16600bc9
Noxbru <brunojimen@gmail.com>
2013-03-07 01:21:11 +0100

    Added the speed test utility
    It measures the speed of each generator
    by generating 10 million random numbers.
    
    Note that when generating 32 bit numbers
    with the 64 bit generators, we have to
    create just 5 million, because a 64 bit
    number can be split in two 32 bits.
    
    Also, the 64 bit numbers are created from
    32 bit generators by concatenating two
    numbers.

---
+++ speed.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#include "rands.h"
+
+void calc_mean_var(double *mean, double *var, unsigned long int *data)
+{
+    unsigned int i;
+
+    *mean=0;
+    for(i = 0; i < 10; i++)
+        *mean+=data[i];
+    *mean/=10;
+
+    *var=0;
+    for(i = 0; i < 10; i++)
+        *var+=(data[i]-*mean)*(data[i]-*mean);
+    *var/=9;
+}
+
+int main(int argc, const char *argv[])
+{
+
+    unsigned int i,j;
+    unsigned long int elapsed_time;
+    unsigned long int times[10];
+    double mean, var;
+
+    struct timespec tp1, tp2;
+
+    printf("#======================================#\n");
+    printf("|    Speed tests for the generators    |\n");
+    printf("#======================================#\n");
+    printf("#======================================#\n");
+    printf("| The tests consists in generating     |\n");
+    printf("| measuring the time it takes to each  |\n");
+    printf("| generator to generate 10 million     |\n");
+    printf("| random numbers                       |\n");
+    printf("+--------------------------------------+\n");
+    printf("| 32 Bits                              |\n");
+    printf("+--------------------------------------+\n");
+
+#if PR_RAND || RANDS_USE_ALL
+    pr_srand(time(NULL));
+    for(j = 0; j < 10; j++)
+    {
+        clock_gettime(CLOCK_MONOTONIC,&tp1);
+        for(i = 0; i < 1e7; i++)
+            pr_rand();
+        clock_gettime(CLOCK_MONOTONIC,&tp2);
+        elapsed_time = (unsigned long) (tp2.tv_sec-tp1.tv_sec)*1000000000 + \
+                       (unsigned long) tp2.tv_nsec-tp1.tv_nsec;
+        times[j] = elapsed_time;
+    }
+    calc_mean_var(&mean,&var,times);
+
+    printf("| Parisi-Rapuano:      %.4lf ± %.4lf |\n",mean/1e9, sqrt(var)/1e9);
+#endif
+
+#if WELL_RAND || RANDS_USE_ALL
+    well_srand(time(NULL));
+    for(j = 0; j < 10; j++)
+    {
+        clock_gettime(CLOCK_MONOTONIC,&tp1);
+        for(i = 0; i < 1e7; i++)
+            well_rand();
+        clock_gettime(CLOCK_MONOTONIC,&tp2);
+        elapsed_time = (unsigned long) (tp2.tv_sec-tp1.tv_sec)*1000000000 + \
+                       (unsigned long) tp2.tv_nsec-tp1.tv_nsec;
+        times[j] = elapsed_time;
+    }
+    calc_mean_var(&mean,&var,times);
+
+    printf("| WELL512:             %.4lf ± %.4lf |\n",mean/1e9, sqrt(var)/1e9);
+#endif
+
+#if WELL_X64_RAND || RANDS_USE_ALL
+    well_x64_srand(time(NULL));
+    for(j = 0; j < 10; j++)
+    {
+        clock_gettime(CLOCK_MONOTONIC,&tp1);
+        for(i = 0; i < 5e6; i++)
+            well_x64_rand();
+        clock_gettime(CLOCK_MONOTONIC,&tp2);
+        elapsed_time = (unsigned long) (tp2.tv_sec-tp1.tv_sec)*1000000000 + \
+                       (unsigned long) tp2.tv_nsec-tp1.tv_nsec;
+        times[j] = elapsed_time;
+    }
+    calc_mean_var(&mean,&var,times);
+
+    printf("| WELL512 (64bits):    %.4lf ± %.4lf |\n",mean/1e9, sqrt(var)/1e9);
+#endif
+
+    // END OF 32 BITS
+    
+    printf("+--------------------------------------+\n");
+    printf("+--------------------------------------+\n");
+    printf("| 64 Bits                              |\n");
+    printf("+--------------------------------------+\n");
+
+#if PR_RAND || RANDS_USE_ALL
+    pr_srand(time(NULL));
+    for(j = 0; j < 10; j++)
+    {
+        clock_gettime(CLOCK_MONOTONIC,&tp1);
+        for(i = 0; i < 2e7; i++)
+            pr_rand();
+        clock_gettime(CLOCK_MONOTONIC,&tp2);
+        elapsed_time = (unsigned long) (tp2.tv_sec-tp1.tv_sec)*1000000000 + \
+                       (unsigned long) tp2.tv_nsec-tp1.tv_nsec;
+        times[j] = elapsed_time;
+    }
+    calc_mean_var(&mean,&var,times);
+
+    printf("| Parisi-Rapuano:      %.4lf ± %.4lf |\n",mean/1e9, sqrt(var)/1e9);
+#endif
+
+#if WELL_RAND || RANDS_USE_ALL
+    well_srand(time(NULL));
+    for(j = 0; j < 10; j++)
+    {
+        clock_gettime(CLOCK_MONOTONIC,&tp1);
+        for(i = 0; i < 2e7; i++)
+            well_rand();
+        clock_gettime(CLOCK_MONOTONIC,&tp2);
+        elapsed_time = (unsigned long) (tp2.tv_sec-tp1.tv_sec)*1000000000 + \
+                       (unsigned long) tp2.tv_nsec-tp1.tv_nsec;
+        times[j] = elapsed_time;
+    }
+    calc_mean_var(&mean,&var,times);
+
+    printf("| WELL512:             %.4lf ± %.4lf |\n",mean/1e9, sqrt(var)/1e9);
+#endif
+
+#if WELL_X64_RAND || RANDS_USE_ALL
+    well_x64_srand(time(NULL));
+    for(j = 0; j < 10; j++)
+    {
+        clock_gettime(CLOCK_MONOTONIC,&tp1);
+        for(i = 0; i < 1e7; i++)
+            well_x64_rand();
+        clock_gettime(CLOCK_MONOTONIC,&tp2);
+        elapsed_time = (unsigned long) (tp2.tv_sec-tp1.tv_sec)*1000000000 + \
+                       (unsigned long) tp2.tv_nsec-tp1.tv_nsec;
+        times[j] = elapsed_time;
+    }
+    calc_mean_var(&mean,&var,times);
+
+    printf("| WELL512 (64bits):    %.4lf ± %.4lf |\n",mean/1e9, sqrt(var)/1e9);
+#endif
+
+    // END OF 64 BITS
+
+    printf("+--------------------------------------+\n");
+    
+    return 0;
+}
